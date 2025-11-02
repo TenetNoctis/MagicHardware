a08038ee584bc6805c27ab576f3c756a0f4da97b
TenetNoctis <noctis.eden@gmail.com>
2025-11-02 20:15:15 +0500

    Splash Screen added

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,10 +1,10 @@
 
 class MagicImages {
-  // -- App Logos
-  static const String darkAppLogo = "assets/logos/magic-store-splash-logo-black.png";
-  static const String lightAppLogo = "assets/logos/magic-store-splash-logo-white.png";
+  // App Logos
+  static const String darkAppLogo = "assets/logos/magic-dark.png";
+  static const String lightAppLogo = "assets/logos/magic.png";
 
-  // -- Social Logos
+  // Social Logos
   static const String google = "assets/logos/google-icon.png";
   static const String facebook = "assets/logos/facebook-icon.png";
 }
